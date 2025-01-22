653152f628c6fb04a917dc922e863fd788425672
syntax-and-codestack <164366136+syntax-and-codestack@users.noreply.github.com>
2025-01-22 10:19:01 -0600

    Create engineafx.h

---
+++ Engine Core/engineafx.h
@@ -0,0 +1,14 @@
+#pragma once
+#ifndef ENGINEAFX_H
+#define ENGINEAFX_H
+
+#include <cassert>
+#include <cctype>
+#include <cfloat>
+#include <cmath>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
+#endif
