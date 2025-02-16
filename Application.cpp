5f2d52575f25d20c3867a65055c81167a4ae26ec
syntax-and-codestack <164366136+syntax-and-codestack@users.noreply.github.com>
2025-02-16 01:26:23 +0000

    add mfc files

---
+++ Application.cpp
@@ -0,0 +1,42 @@
+#ifndef APPLICATION_MFC_H
+#define APPLICATION_MFC_H
+
+#ifndef _DEBUG_MFC_APPLICATION 
+#define _DEBUG_MFC_APPLICATION 
+
+#define MFC_VERSION 1.0
+#define APPLICATION_VERSION >> MFC_VERSION
+
+#define entry int
+
+#define APPLICATION_MSVC_CPP "msvc81.dll"
+
+#define MFC_SQL_INFO "mfcsql781.dll"
+#define MFC_APPLICATION_DLL "mfckernel71.dll"
+#define MFC_STRING_DLL "mfcstr.dll"
+
+#define MSVC_CPP_COMPILER "msvc81.dll" 
+
+#define MFC_APPLICATION_VER "mfc42.dll"
+
+#define MFC_APPLICATION_ENU "MFC90ENU.DLL"
+#define MFC_APPLICATION_ESP "MFC90ESP.DLL"
+#define MFC_APPLICATION_FRA "MFC90FRA.DLL"
+#define MFC_APPLICATION_ITA "MFC90ITA.DLL"
+#define MFC_APPLICATION_JPN "MFC90JPN.DLL"
+#define MFC_APPLICATION_KOR "MFC90KOR.DLL"
+#define MFC_APPLICATION_RUS "MFC90RUS.DLL"
+
+#define MFC_APPLICATION_CAPTURE_ENGINE "MFCaptureEngine.dll"
+#define MFCM_80 "mfcm80.dll"
+#define MFCM_90 "mfcm90.dll"
+#define MFC_APPLICATION_CORE "mfcore.dll"
+#define MFC_APPLICATION_SUBS "mfcsubs.dll"
+
+#endif
+
+entry WinMain{
+    
+};
+
+#endif
